2bd04b68e4f934f1acce3055662c201db5def095
louicoder <musanje2010@gmail.com>
2021-05-03 09:55:05 +0300

    refactor blogs model to make likes and comments an array

--- Models/Blogs.js
+++ Models/Blogs.js
@@ -6,7 +6,9 @@ const BlogSchema = new mongoose.Schema({
   owner: { type: { email: String, imageUrl: String } },
   imageUrl: String,
   dateCreated: String,
-  tags: { type: Array }
+  tags: { type: Array },
+  likes: { type: [ { uid: String, imageUrl: String, name: String, email: String } ] },
+  comments: { type: [ { uid: String, imageUrl: String, name: String, email: String } ] }
 });
 
 module.exports = mongoose.model('blogs', BlogSchema);
