f041ea8473e233b31af44c69bc6ea76008096155
louicoder <musanje2010@gmail.com>
2021-05-03 09:55:40 +0300

    Refactor events model to make likes and comments an array

--- Models/Events.js
+++ Models/Events.js
@@ -25,10 +25,10 @@ const eventSchema = new mongoose.Schema({
     default: 'judges'
   },
   judgingNotes: String,
-  attending: { type: { uid: String, imageUrl: String, name: String, email: String } },
-  participants: { type: { uid: String, imageUrl: String, name: String, email: String } },
-  likes: { type: { uid: String, imageUrl: String, name: String, email: String } },
-  comments: { type: { uid: String, imageUrl: String, name: String, email: String } }
+  attending: { type: [ { uid: String, imageUrl: String, name: String, email: String } ] },
+  participants: { type: [ { uid: String, imageUrl: String, name: String, email: String } ] },
+  likes: { type: [ { uid: String, imageUrl: String, name: String, email: String } ] },
+  comments: { type: [ { uid: String, imageUrl: String, name: String, email: String } ] }
 });
 
 module.exports = mongoose.model('events', eventSchema);
